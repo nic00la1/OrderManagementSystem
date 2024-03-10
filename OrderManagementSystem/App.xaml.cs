5e9aee7785658ec93e3b669e12c01ced9ce27410
nic00la1 <nic00la.kaleta@gmail.com>
2024-03-10 10:35:37 +0100

    Add project files.

---
+++ OrderManagementSystem/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderManagementSystem
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ OrderManagementSystem/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderManagementSystem
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
