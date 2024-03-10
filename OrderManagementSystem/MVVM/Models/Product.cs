6886df1ecea6d327a5f9c3f1a00b3662a1262bfb
nic00la <nic00la.kaleta@gmail.com>
2024-03-10 11:08:23 +0100

    Simple UI, and Product model ✅

---
+++ OrderManagementSystem/MVVM/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿namespace OrderManagementSystem.MVVM.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public string[]? Category { get; set; }
+        public string? Description { get; set; }
+
+        public Product( // Constructor
+            int id,
+            string? name,
+            decimal price,
+            string[]? category,
+            string? description
+            )
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Category = category;
+            Description = description;
+        }
+    }
+}
---
+++ OrderManagementSystem/MVVM/Views/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderManagementSystem
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
