94e97ae63496832fdcfcc81c01b03164b1debcf0
nic00la <nic00la.kaleta@gmail.com>
2024-03-10 12:30:17 +0100

    Dodawanie produktu + wyswietlanie

--- OrderManagementSystem/MVVM/Models/Product.cs
+++ OrderManagementSystem/MVVM/Models/Product.cs
@@ -2,25 +2,20 @@
 {
     public class Product
     {
-        public int Id { get; set; }
+        private static int nextId = 1;
+
+        public int Id { get; private set; }
         public string? Name { get; set; }
         public decimal Price { get; set; }
-        public string[]? Category { get; set; }
-        public string? Description { get; set; }
+        public string? Category { get; set; }
 
-        public Product( // Constructor
-            int id,
-            string? name,
-            decimal price,
-            string[]? category,
-            string? description
-            )
+        // Constructor
+        public Product(string? name, decimal price, string? category)
         {
-            Id = id;
+            Id = nextId++;
             Name = name;
             Price = price;
             Category = category;
-            Description = description;
         }
     }
 }
--- OrderManagementSystem/MVVM/Views/MainPage.xaml.cs
+++ OrderManagementSystem/MVVM/Views/MainPage.xaml.cs
@@ -1,13 +1,43 @@
-﻿namespace OrderManagementSystem
+﻿using OrderManagementSystem.MVVM.Models;
+
+namespace OrderManagementSystem
 {
     public partial class MainPage : ContentPage
     {
+        private List<Product> products = new List<Product>();
 
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private void AddProduct_Clicked(object sender, EventArgs e)
+        {
+            string name = ProductNameEntry.Text;
+            decimal price = Convert.ToDecimal(ProductPriceEntry.Text);
+            string? category = KategoriaPicker.SelectedItem.ToString();
+
+            Product newProduct = new Product(name, price, category);
+            products.Add(newProduct);
+
+            DisplayProducts();
+        }
+
+        private void DisplayProducts()
+        {
+            ProductsStackLayout.Children.Clear();
+
+            foreach (var product in products)
+            {
+                var label = new Label
+                {
+                    Text = $"Nazwa: {product.Name}, Cena: {product.Price}, Kategoria: {product.Category}",
+                    FontSize = 16
+                };
+
+                ProductsStackLayout.Children.Add(label);
+            }
+        }
     }
 
 }
